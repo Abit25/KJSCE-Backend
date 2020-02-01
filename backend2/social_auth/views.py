046465a0c0292b384cb7a7b6c0687a01535a7117
Abit25 <abheet25@gmail.com>
2020-02-01 13:05:05 +0530

    Ported to Django

---
+++ backend2/social_auth/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('http://localhost:3000')
---
+++ myenv2/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/priya/Desktop/KJSCE/server-django/myenv2/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
